fffd2cde8a9b6825b276db7848d1a4a5ea8838b5
xh1y <xhily_@outlook.com>
2024-07-20 21:18:05 +0800

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+/**
+ * Create time: 2024 07 20  21:10:32
+ * File name: App.tsx
+ * Path: src\App.tsx
+ * About: App page
+ */
+
+function App() {
+    return (
+        <div>
+            <h2>App</h2>
+        </div>
+    )
+}
+
+export default App
+// App
